9c123b6a5dd3c730947ba51cff738c0c22b36c94
BigFish2086 <a8686.ibrahim@gmail.com>
2023-10-09 04:06:06 +0300

    finish seq! up to test 04

--- main.rs
+++ main.rs
@@ -9,8 +9,22 @@
 use seq::seq;
 
 seq!(N in 1..4 {
-    compile_error!(concat!("error number ", stringify!(N)));
+    fn f~N () -> u64 {
+        N * 2
+    }
 });
 
+// This f0 is written separately to detect whether your macro correctly starts
+// with the first iteration at N=1 as specified in the invocation. If the macro
+// incorrectly started at N=0 like in the previous tests cases, the first
+// generated function would conflict with this one and the program would not
+// compile.
+fn f0() -> u64 {
+    100
+}
+
 fn main() {
+    let sum = f0() + f1() + f2() + f3();
+
+    assert_eq!(sum, 100 + 2 + 4 + 6);
 }
--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -25,7 +25,7 @@ impl Parse for SeqMacroInput {
         let content;
         let _braced = syn::braced!(content in input);
         let ts = TokenStream2::parse(&content)?;
-        eprintln!("ts: {:#?}", ts);
+        // eprintln!("ts: {:#?}", ts);
 
         Ok(SeqMacroInput {
             ident,
@@ -47,9 +47,14 @@ impl Into<TokenStream2> for SeqMacroInput {
 }
 
 impl SeqMacroInput {
-    fn expand_token_tree(&self, stream: TokenTree2, i: u64) -> TokenTree2 {
+    fn expand_token_tree(
+        &self,
+        stream: TokenTree2,
+        rest: &mut proc_macro2::token_stream::IntoIter,
+        i: u64,
+    ) -> TokenTree2 {
         match stream {
-            TokenTree2::Group(group) => {
+            TokenTree2::Group(ref group) => {
                 let mut expanded = proc_macro2::Group::new(
                     group.delimiter(),
                     self.expand_token_stream(group.stream(), i),
@@ -63,15 +68,37 @@ impl SeqMacroInput {
                 lit.set_span(ident.span());
                 TokenTree2::Literal(lit)
             }
+            TokenTree2::Ident(mut ident) => {
+                let mut peek = rest.clone();
+                match (peek.next(), peek.next()) {
+                    // example: `func~N`
+                    (Some(TokenTree2::Punct(ref punct)), Some(TokenTree2::Ident(ref ident2)))
+                        if punct.as_char() == '~' && ident2 == &self.ident => {
+                            ident = proc_macro2::Ident::new(&format!("{}{}", ident, i), ident.span()) .into();
+                            *rest = peek.clone();
+                            match peek.next() {
+                                // to consume another '~' if found, example: `func~N~`
+                                Some(TokenTree2::Punct(ref punct)) if punct.as_char() == '~' => {
+                                    *rest = peek;
+                                }
+                                _ => {}
+                            }
+                        }
+                    _ => {}
+                };
+                TokenTree2::Ident(ident)
+            }
             tt => tt,
         }
     }
 
     fn expand_token_stream(&self, stream: TokenStream2, i: u64) -> TokenStream2 {
-        stream
-            .into_iter()
-            .map(|tt| self.expand_token_tree(tt, i))
-            .collect()
+        let mut out = TokenStream2::new();
+        let mut tts = stream.into_iter();
+        while let Some(tt) = tts.next() {
+            out.extend(std::iter::once(self.expand_token_tree(tt, &mut tts, i)));
+        }
+        out.into()
     }
 }
 
